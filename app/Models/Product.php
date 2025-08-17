f78cfa5a590bf10c1ff62f0a8b60164804e6ae8d
Raihan Nismara <raihannismara@gmail.com>
2025-08-17 23:11:54 +0700

    chore: update fillable model

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock',
+        'sku',
+        'image',
+        'is_active',
+    ];
 }
